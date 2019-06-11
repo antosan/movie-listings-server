c49a588ab46ba89465fa1e01fe3d3f979ba300b4
Antony Sande <sandeantony@gmail.com>
2019-06-11 12:10:48 +0300

    Add /api/cinema route

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require("express");
+const mysql = require("mysql");
 const app = express();
+const pool = mysql.createPool({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "movie_listings"
+});
 
-app.get("/", (req, res) => {
-    // Logic
-    res.json({ message: "Hello world" });
+app.get("/api/cinemas", (req, res) => {
+    // Fetch movies from the database
+    pool.query("SELECT id, name FROM cinema", (error, rows) => {
+        if (error) {
+            return res.status(500).json({ error });
+        }
+
+        res.json(rows);
+    });
 });
 
 app.listen(9000, function() {
