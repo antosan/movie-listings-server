951aa16ee3b4d91d8720acbb86f82d58ed91d75f
Antony Sande <sandeantony@gmail.com>
2019-06-18 10:22:34 +0300

    Add /api/cinemas/:id endpoint

--- index.js
+++ index.js
@@ -21,6 +21,20 @@ app.get("/api/cinemas", (req, res) => {
     });
 });
 
+app.get("/api/cinemas/:id", (req, res) => {
+    pool.query(
+        "SELECT id, name FROM cinema WHERE id = ?",
+        [req.params.id],
+        (error, rows) => {
+            if (error) {
+                return res.status(500).json({ error });
+            }
+
+            res.json(rows);
+        }
+    );
+});
+
 app.listen(9000, function() {
     console.log("App listening on port 9000");
 });
