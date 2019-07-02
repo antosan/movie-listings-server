977fd7850fb1b60ac02bbbd609d77f1db2a3bf44
Antony Sande <sandeantony@gmail.com>
2019-07-02 12:27:11 +0300

    Add state to MovieList

--- client/src/App.js
+++ client/src/App.js
@@ -3,27 +3,6 @@ import NavBar from "./NavBar";
 import MovieList from "./MovieList";
 import "./App.css";
 
-const MOVIES_SHOWING = [
-    {
-        id: 2,
-        title: "Kalank",
-        poster_url: "http://bit.ly/2WnGRdu",
-        cinema_count: 2
-    },
-    {
-        id: 1,
-        title: "Captain Marvel",
-        poster_url: "http://bit.ly/2vI1c1n",
-        cinema_count: 1
-    },
-    {
-        id: 4,
-        title: "Movie with no name Movie with no name",
-        poster_url: "invalid",
-        cinema_count: 0
-    }
-];
-
 function App() {
     return (
         <div className="mvls-app">
@@ -31,7 +10,7 @@ function App() {
                 <NavBar />
             </header>
             <main className="mvls-main">
-                <MovieList moviesShowing={MOVIES_SHOWING} />
+                <MovieList />
             </main>
         </div>
     );
--- client/src/MovieList.js
+++ client/src/MovieList.js
@@ -1,16 +1,25 @@
 import React from "react";
 import Movie from "./Movie";
 
-function MovieList({ moviesShowing }) {
-    return (
-        <div className="mvls-container">
-            <div className="mvls-movie-list">
-                {moviesShowing.map(m => (
-                    <Movie key={m.id} movieShowing={m} />
-                ))}
+class MovieList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            moviesShowing: []
+        };
+    }
+
+    render() {
+        return (
+            <div className="mvls-container">
+                <div className="mvls-movie-list">
+                    {this.state.moviesShowing.map(m => (
+                        <Movie key={m.id} movieShowing={m} />
+                    ))}
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default MovieList;
