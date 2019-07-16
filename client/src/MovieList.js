91de14454452caab8cfad0ffa82436dcd7427d71
Antony Sande <sandeantony@gmail.com>
2019-07-16 09:31:43 +0300

    Fetch movies from database

--- client/src/MovieList.js
+++ client/src/MovieList.js
@@ -1,19 +1,60 @@
 import React from "react";
+import axios from "axios";
 import Movie from "./Movie";
 
 class MovieList extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            moviesShowing: []
+            moviesShowing: [],
+            loading: false,
+            error: false
         };
     }
 
+    componentDidMount() {
+        this.fetchMoviesShowing();
+    }
+
+    fetchMoviesShowing() {
+        this.setState({
+            loading: true,
+            error: false
+        });
+
+        axios
+            .get("/api/moviesshowing")
+            .then(response => {
+                this.setState({
+                    moviesShowing: response.data,
+                    loading: false,
+                    error: false
+                });
+            })
+            .catch(error => {
+                this.setState({
+                    moviesShowing: [],
+                    loading: false,
+                    error: true
+                });
+            });
+    }
+
     render() {
+        const { moviesShowing, loading, error } = this.state;
+
+        if (loading) {
+            return <h1>Loading...</h1>;
+        }
+
+        if (error) {
+            return <h1>Something went wrong...</h1>;
+        }
+
         return (
             <div className="mvls-container">
                 <div className="mvls-movie-list">
-                    {this.state.moviesShowing.map(m => (
+                    {moviesShowing.map(m => (
                         <Movie key={m.id} movieShowing={m} />
                     ))}
                 </div>
